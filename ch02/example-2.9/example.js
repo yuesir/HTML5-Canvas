09814ba9a808880e5440563bc32ab5d8892b911e
hehongwei44 <hehongwei44@126.com>
2014-08-24 22:01:33 +0800

    canva一个完整的绘图程序(待研究)

--- ch02/example-2.9/example.js
+++ ch02/example-2.9/example.js
@@ -1,40 +1,11 @@
-/*
- * Copyright (C) 2012 David Geary. This code is from the book
- * Core HTML5 Canvas, published by Prentice-Hall in 2012.
- *
- * License:
- *
- * Permission is hereby granted, free of charge, to any person 
- * obtaining a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * The Software may not be used to create training material of any sort,
- * including courses, books, instructional videos, presentations, etc.
- * without the express written consent of David Geary.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
-*/
 
 var context = document.getElementById('canvas').getContext('2d');
 
 // Functions..........................................................
 
+//绘制网格的函数
 function drawGrid(context, color, stepx, stepy) {
-   context.save()
+   context.save();
 
    context.strokeStyle = color;
    context.lineWidth = 0.5;
@@ -75,7 +46,7 @@ context.fillText('Fill', 440, 110);
 context.strokeText('Stroke & Fill', 650, 110);
 context.fillText('Stroke & Fill', 650, 110);
 
-// Rectangles.........................................................
+// 花矩形.........................................................
 
 context.lineWidth = '5';       // line width set to 5 for shapes
 context.beginPath();
